3178145a99a5cbde64ac581541e0d0898537e837
BruceManuelwayne <47195508+BruceManuelwayne@users.noreply.github.com>
2020-09-06 14:15:42 -0300

    error llamada de Validar palabra

---
+++ Ejercicio4posta/Ejercicio4posta/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4posta
+{
+    class Program
+    {
+
+
+     
+
+        
+        static class Ordenar
+        {
+            public static string OrdenarString(string str1)
+            {
+                char[] array1 = str1.ToArray();
+                Array.Sort(array1);
+                return new string(array1);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            string string1;
+            string string2;
+            bool flag;
+
+            do
+            {
+                Console.WriteLine("Ingrese la primera palabra: ");
+                string1 = Console.ReadLine();
+
+                flag = ValidarPalabra(string1); //error
+
+            }
+            while (flag == false);
+
+            do
+            {
+                Console.WriteLine("Ingrese la primera palabra: ");
+                string2 = Console.ReadLine();
+
+                flag = new ValidarPalabra(string2); //error
+
+
+            }
+            while (flag == false);
+
+            Console.WriteLine("el valor de la primera palabra es :{0}", string1);
+            Console.WriteLine("El valor de la segunda palabra es :{0}", string2);
+
+            string strOrdenado = Ordenar.OrdenarString(string1);
+            string strOrdenado2 = Ordenar.OrdenarString(string2);
+
+            Console.WriteLine("La primera palabra ordenada es:{0}", strOrdenado);
+            Console.WriteLine("La segunda palabra ordenada es:{0}", strOrdenado2);
+
+            if (strOrdenado == strOrdenado2)
+            {
+                Console.WriteLine("La palabra{0} es anagrama de la palabra{1}:", string1, string2);
+
+            }
+
+            else
+            {
+                Console.WriteLine("La palabra{0} no es anagrama de la palabra{1}:", string1, string2);
+            }
+
+
+        }
+    }
+
+}
---
+++ ejerRepaso4/ejerRepaso4/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejerRepaso4
+{
+    class Program
+    {
+        private bool ValidarPalabra(string NroRegistro)
+        {
+            bool flag = false;
+
+            if (String.IsNullOrEmpty(NroRegistro) || String.IsNullOrWhiteSpace(NroRegistro))
+            {
+                Console.WriteLine("Debe ingresar una palabra para validar");
+            }
+            else
+            {
+                flag = true;
+            }
+            return flag;
+        }
+        static class Invertir
+        {
+            public static string ReverseString(string myStr)
+            {
+                char[] myArr = myStr.ToCharArray();
+                Array.Reverse(myArr);
+                return new string(myArr);
+            }
+        }
+
+        static class Ordenar
+        {
+            public static string OrdenarString(string myStr)
+            {
+                char[] myArr = myStr.ToCharArray();
+                Array.Sort(myArr);
+                return new string(myArr);
+            }
+        }
+
+        public void main()
+        {
+            string StrRegistro;
+            string StrRegistro2;
+            bool flag;
+
+            do
+            {
+                Console.WriteLine("Por favor ingrese la primer palabra: ");
+                StrRegistro = Console.ReadLine();
+
+                flag = ValidarPalabra(StrRegistro);
+
+            } while (flag == false);
+
+            do
+            {
+                Console.WriteLine("Por favor ingrese la segunda palabra: ");
+                StrRegistro2 = Console.ReadLine();
+
+                flag = ValidarPalabra(StrRegistro2);
+
+            } while (flag == false);
+
+            Console.WriteLine("el valor de la primer palabra es: {0}", StrRegistro);
+            Console.WriteLine("el valor de la segunda palabra es: {0}", StrRegistro2);
+
+            string StrOrdenado = Ordenar.OrdenarString(StrRegistro);
+            string StrOrdenado2 = Ordenar.OrdenarString(StrRegistro2);
+            Console.WriteLine("La primer palabra ordenada es: {0}", StrOrdenado);
+            Console.WriteLine("La segunda palabra ordenada es: {0}", StrOrdenado2);
+
+            if (StrOrdenado == StrOrdenado2)
+            {
+                Console.WriteLine("La palabra {0} es anagrama de la palabra {1}: ", StrRegistro, StrRegistro2);
+            }
+
+            else
+            {
+                Console.WriteLine("La palabra {0} no es anagrama de la palabra {1}: ", StrRegistro, StrRegistro2);
+            }
+
+        }
+    }
+}
+
