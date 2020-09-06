e583502c935993115e27e5ab2108f27fe93f21c3
BruceManuelwayne <47195508+BruceManuelwayne@users.noreply.github.com>
2020-09-06 15:35:56 -0300

    numero invertido

---
+++ Ejercicio5Repaso/Ejercicio5Repaso/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5Repaso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroIngresado;
+          
+            int t;
+            int sum = 0;
+            int r; 
+            
+            
+            Console.WriteLine("Ingrese un numero: ");
+
+            var numeroComoString = Console.ReadLine();
+
+            //validacion
+            if (int.TryParse(numeroComoString, out numeroIngresado))
+            {
+                Console.WriteLine($"Numero ingresado es : {numeroIngresado}");
+                for (t = numeroIngresado; t != 0; t = t / 10)
+                {
+                    r = t % 10;
+                    sum = sum * 10 + r;
+                }
+                Console.Write("El numero en orden inverso es : {0} \n", sum);
+
+            }
+            else
+            {
+                Console.WriteLine("No es un numero"); 
+            }
+
+            
+
+
+        }
+    }
+}
