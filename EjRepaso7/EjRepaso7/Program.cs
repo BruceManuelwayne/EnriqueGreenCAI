15059baf1ffbde4cc723c3457492965964b97adb
BruceManuelwayne <47195508+BruceManuelwayne@users.noreply.github.com>
2020-09-12 15:55:20 -0300

    commit ej7

---
+++ EjRepaso7/EjRepaso7/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjRepaso7
+{
+    class Program
+    {
+        public class Validacion
+        {
+            public static int pedirNumero(string mensaje, int minimo, int max)
+            {
+                int num = 0;
+                do
+                {
+                    Console.Write("Ingrese " + mensaje + ": ");
+                    if (!(int.TryParse(Console.ReadLine(), out num)))
+                    {
+                        num = -1;
+                        Console.WriteLine("Error. Reingrese un valor numerico.");
+                    }
+                    else if (num < minimo || num > max)
+                    {
+                        Console.WriteLine("Error. El numero debe estar entre " + minimo + " y " + max);
+                    }
+                } while (num < minimo || num > max);
+                return num;
+            }
+            static void Main(string[] args)
+            {
+
+            }
+        }
+    }
+}
