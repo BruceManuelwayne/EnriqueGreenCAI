b324b7a845b3abd0e78b0a2531914794bcfad903
BruceManuelwayne <47195508+BruceManuelwayne@users.noreply.github.com>
2020-09-16 21:24:58 -0300

    Update Program.cs

--- Repaso8-20/Ej10/Program.cs
+++ Repaso8-20/Ej10/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Repaso8_20;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,34 @@ namespace Ej10
     {
         static void Main(string[] args)
         {
+            char letra;
+
+            bool valida = true;
+
+            do
+            {
+                if(!char.TryParse(Validaciones.pedirTexto("una Tecla").ToLower(), out letra))
+                {
+                    Console.WriteLine("Error, debe ingresar solo una tecla. Intente de nuevo: ");
+                }
+
+            } while (!valida);
+
+            if ("aeiou".Contains(letra))
+            {
+                Console.WriteLine(letra + " es una vocal");
+            }
+            else if ("bcdfghjklmnpqrstvwxyz".Contains(letra)) 
+            {
+                Console.WriteLine(letra + " es una consonante"); 
+            }
+            else if ("1234567890".Contains(letra)){
+                Console.WriteLine(letra + " es un numero"); 
+            }
+            else
+            {
+                Console.WriteLine(letra + " no es una vocal, consonante o numero"); 
+            }
         }
     }
 }
