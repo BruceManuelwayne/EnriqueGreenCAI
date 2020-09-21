a674839ed9fa0b7d53810461f02b1c085f04d9fb
BruceManuelwayne <47195508+BruceManuelwayne@users.noreply.github.com>
2020-09-20 22:16:34 -0300

    ejercicio 15

---
+++ Repaso8-20/Ej15/Program.cs
@@ -0,0 +1,51 @@
+﻿using Repaso8_20;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intentos = 0;
+            string clave = "enrique";
+            bool claveCorrecta = false;
+            string ingreso = "";
+
+
+            do
+            {
+                
+                     ingreso = Validaciones.pedirTexto("usuario");
+                                         if (intentos == 2)
+                                        {
+                                            Console.WriteLine("Usuario Bloqueado, superaste intentos  posibles");
+                                            break;
+                                        }
+                                        if (String.Compare(clave, ingreso) == 0)
+                                        {
+                                            Console.WriteLine("Bienvenido *{0}", clave);
+                                            claveCorrecta = true;
+                                        }
+                    
+                                        else
+                                        {
+                                            Console.WriteLine("Acceso Denegado, La contraseña no es correct.");
+                                            intentos++;
+                                        }
+
+                
+            } while (!claveCorrecta || intentos < 2);
+        }
+        
+    }
+}
+
+
+        
+    
+
