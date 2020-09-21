3695a6958b32f27fa2fa40f329e61b18a01d0093
BruceManuelwayne <47195508+BruceManuelwayne@users.noreply.github.com>
2020-09-20 23:01:58 -0300

    ej 17

---
+++ Repaso8-20/ej16/Program.cs
@@ -0,0 +1,20 @@
+﻿using Repaso8_20;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ej16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            numero = Validaciones.pedirNumero("Numero", 0, 9999999);
+            Console.WriteLine("Numero Ingresado: {0} tiene {1} numeros.", numero, numero.ToString().Length); 
+        }
+    }
+}
---
+++ Repaso8-20/ej17/Validaciones.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repaso8_20
+{
+    static class Validaciones
+    {
+        public static int pedirNumero(string mensaje, int minimo, int max)
+        {
+            int num = 0;
+            do
+            {
+                Console.Write("Ingrese " + mensaje + ": ");
+                if (!(int.TryParse(Console.ReadLine(), out num)))
+                {
+                    num = -1;
+                    Console.WriteLine("Error. Reingrese un valor numerico.");
+                }
+                else if (num < minimo || num > max)
+                {
+                    Console.WriteLine("Error. El numero debe estar entre " + minimo + " y " + max);
+                }
+            } while (num < minimo || num > max);
+            return num;
+        }
+        public static string pedirTexto(string mensaje)
+        {
+            string text = "";
+
+            do
+            {
+                Console.WriteLine("ingrese " + mensaje + ": ");
+                text = Console.ReadLine();
+                if (string.IsNullOrEmpty(text))
+                    Console.WriteLine("Ingrese texto valido: ");
+
+            } while (string.IsNullOrEmpty(text));
+            return text;
+        }
+        public static double pedirDouble(string mensaje, double min, double max)
+        {
+            double resultado = 0;
+            do
+            {
+                Console.Write("Ingrese " + mensaje + ": ");
+                if (!double.TryParse(Console.ReadLine(), out resultado))
+                {
+                    Console.WriteLine("Error. Reingrese un valor numerico.");
+                }
+                else if (resultado < min || resultado > max)
+                {
+                    Console.WriteLine("Error. El numero debe estar entre " + min + " y " + max);
+                }
+            } while (resultado < min || resultado > max);
+
+            return resultado;
+        }
+
+        public static string pedirContinuar()
+        {
+            string texto = "";
+            bool estaOk = false;
+            do
+            {
+                Console.Write("Desea probar otro ejercicio? 'S' para SI o 'N' para salir de la app.");
+                texto = Console.ReadLine().ToUpper();
+                if (string.IsNullOrEmpty(texto))
+                    Console.WriteLine("Por favor, ingrese un texto");
+                if (string.Compare(texto, "S") == 0)
+                    estaOk = true;
+                if (string.Compare(texto, "N") == 0)
+                    estaOk = true;
+                if (!estaOk)
+                    Console.WriteLine("Por favor, ingrese S o N");
+            } while (!estaOk);
+            return texto;
+        }
+
+        public static DateTime pedirFecha(string mensaje)
+        {
+            DateTime valor;
+            bool ok = false;
+            do
+            {
+                if(!DateTime.TryParse(Validaciones.pedirTexto(mensaje), out valor))
+                {
+                    Console.WriteLine("Error. Infresar una fecha: ");
+                }
+                else
+                {
+                    ok = true;
+                }
+
+            } while (!ok);
+            return valor; 
+        }
+
+    }
+}
