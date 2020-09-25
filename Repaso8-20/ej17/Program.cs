3ecf09b7f71143b225c2445adc513f437749054c
BruceManuelwayne <47195508+BruceManuelwayne@users.noreply.github.com>
2020-09-25 01:01:55 -0300

    ej17
    lo queria hacer con classes y exceptions pero no me salio.

--- Repaso8-20/ej17/Program.cs
+++ Repaso8-20/ej17/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Repaso8_20;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,27 @@ namespace ej17
     {
         static void Main(string[] args)
         {
+
+            const int cantidad = 5;
+            const double sueldoMax = 1000000;
+            double[] sueldo = new double[cantidad];
+            string[] nombre = new string[cantidad];
+            double maximo = 0;
+            int auxMax = 0; 
+
+            for (int i = 0; i< cantidad; i++)
+            {
+
+                nombre[i] = Validaciones.pedirTexto("Nombre del empeado" + (i + 1));
+                sueldo[i] = Validaciones.pedirDouble("sueldo ", 0, sueldoMax); 
+
+                if (sueldo[i] > maximo)
+                {
+                    maximo = sueldo[i];
+                    auxMax = i; 
+                }
+            }
+            Console.WriteLine("El empleado con mayor sueldo es -Nombre: {0}, Sueldo: {1}", nombre[auxMax], sueldo[auxMax]); 
         }
     }
 }
---
+++ Repaso8-20/ej17/persona.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ej17
+{
+    public class persona
+    {
+        public string Nombre { get; set;  }
+
+        public string Apellido { get; set;  }
+
+        public Double Sueldo { get; set;  }
+
+       
+      
+        
+
+        public List<persona> allPersonas = new List<persona>(); 
+
+        public persona(string nombre, string apellido, double sueldo)
+        {
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+            this.Sueldo = sueldo; 
+
+        }
+
+        
+
+    }
+}
